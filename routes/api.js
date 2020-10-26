03f0071b79232a3231d8e33c0a217b0c95eb0b94
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-25 23:40:42 -0400

    re initialized package.json

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,30 @@
-const express = require ('express');
+const { RSA_NO_PADDING } = require('constants');
+const express = require('express');
+const Todo = require('../models/Model');
 const router = express.Router();
 
 router.get('/todos', (req, res, next) => {
-
+    Todo.find({}, 'action')
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 router.post('/todos', (req, res, next) => {
-
+    if (req.body.action) {
+        Todo.create(req.body)
+            .then(data => res.json(data))
+            .catch(next)
+    } else {
+        res.json({
+            error: "input field is empty"
+        })
+    }
 });
 
 router.delete('/todos/:id', (req, res, next) => {
-
+    Todo.findOneAndDelete({ "_id": req.params.id })
+        .then(data => res.json(data))
+        .catch(next)
 })
 
 module.exports = router;
